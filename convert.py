07fb313c517741c9e1ef220afb40364aab2e0805
Mike Abbott <furrysalamander@gmail.com>
2023-04-29 13:14:59 -0600

    removing unused code

--- convert.py
+++
@@ -1,5 +0,0 @@
-from glob import glob
-import os
-from pathlib import Path
-for file_name in glob("sample_data/*"):
-    os.system(f"ffmpeg -i {file_name} gif/{Path(file_name).stem}.gif")
--- pa_scan.py
+++
@@ -1,7 +0,0 @@
-from pattern_info import PatternInfo
-
-class PaScan:
-    def __init__(self, pattern_info: PatternInfo, video_files) -> None:
-        self.pattern_info = pattern_info
-        self.video_files = video_files
-        pass
